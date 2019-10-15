4ceff463f0ef36cf977d88dc18beb0cd1e38855b
CHRISTOPER NICOLAS (DOCU-IRIS-ISD-OOCLL/MNL) <NICOLCH@OOCL.COM>
2019-10-15 21:58:06 +0800

    Completed all requirements

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -0,0 +1,47 @@
+package com.tw.apistackbase.controller;
+import com.tw.apistackbase.model.EmployeeModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeResource {
+    private List<EmployeeModel> employeeList = new ArrayList<>();
+
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public List<EmployeeModel> add(@RequestBody EmployeeModel employee) {
+        employeeList.add(employee);
+        return employeeList;
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<List<EmployeeModel>> delete(@PathVariable Integer id){
+        EmployeeModel emp = this.employeeList.stream().filter(element -> element.getId().equals(id)).findFirst().orElse(null);
+        employeeList.remove(emp);
+        return ResponseEntity.ok(employeeList);
+    }
+
+    @PutMapping(path = "/change/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<List<EmployeeModel>> change(@RequestBody EmployeeModel employee, @PathVariable Integer id){
+        EmployeeModel emp = this.employeeList.stream().filter(element -> element.getId().equals(id)).findFirst().orElse(null);
+
+        emp.setGender(employee.getGender());
+        emp.setName(employee.getName());
+        emp.setAge(employee.getAge());
+
+        return ResponseEntity.ok(employeeList);
+    }
+
+    @GetMapping(path = "/search", produces = {"application/json"})
+    public ResponseEntity<List> search() {
+        return ResponseEntity.ok(employeeList);
+    }
+
+    @GetMapping(path = "/search/{id}", produces = {"application/json"})
+    public ResponseEntity<Stream<EmployeeModel>> search(@PathVariable Integer id){
+        return ResponseEntity.ok(employeeList.stream().filter(element -> element.getId().equals(id)));
+    }
+}
---
+++ src/main/java/com/tw/apistackbase/model/EmployeeModel.java
@@ -0,0 +1,40 @@
+package com.tw.apistackbase.model;
+
+public class EmployeeModel {
+    private String gender;
+    private String name;
+    private Integer id;
+    private Integer age;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
